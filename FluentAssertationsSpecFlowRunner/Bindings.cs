1711a1186b1385395058a362e59e62b357fa4980
Andreas Willich <andreas.willich@techtalk.at>
2017-10-03 13:45:29 +0200

    code

---
+++ FluentAssertationsSpecFlowRunner/Bindings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace FluentAssertationsSpecFlowRunner
+{
+    [Binding]
+    public class Bindings
+    {
+        private readonly List<int> _numbers = new List<int>();
+        private int _sum;
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            _numbers.Add(p0);
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            _sum = _numbers.Sum();
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            _sum.Should().Be(p0);
+        }
+    }
+}
